83b994be93db3dfc6fcd688c78c7378708ffa127
songpeng22 <songpeng24@msn.com>
2024-06-12 11:00:12 +0800

    windows form with debug output message

---
+++ 01_WindowsForm_output_debug_message/WindowsFormsApplication1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//support Debug.WriteLine()
+using System.Diagnostics;
+
+namespace WindowsFormsApplication1
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Console.WriteLine("Main()...............1");
+            Debug.WriteLine("Main()...............2");
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
